8364f246aa1a7d255fce142a8f10f2d732b10e88
Matt Neuburg <matt@tidbits.com>
2021-04-13 09:44:22 +1200

    init

---
+++ TwoStoryboards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoStoryboards
+//
+//  Created by Matt Neuburg on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
